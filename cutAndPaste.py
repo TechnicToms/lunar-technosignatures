fc3e7a8f460d6dd93365e36e30de8ee6df19b366
Tom Sander <tom96.sander@t-online.de>
2024-10-23 14:40:56 +0000

    final touches

--- cutAndPaste.py
+++ cutAndPaste.py
@@ -14,104 +14,11 @@ from transforms.cutPasteTransform import CutPasteTransform
 from transforms.cutTransform import CutPatchTransform, CutPatchMeanTransform
 from transforms.darkenTransform import DarkenTransform, DarkenBlobTransform, WhitenBlobTransform
 
-from models.cp_transformer import ViT
-
 import torchmetrics
 from evaluateModel import evaluateLandingSite
 
 from helpers.terminalColor import terminalColor as tc
 
-
-class CutAndPasteViT(pl.LightningModule):
-    def __init__(self, img_size: int, patch_size: int, cut_patch_size: int):
-        super().__init__()
-        self.transformer = ViT(img_size=img_size, patch_size=patch_size)
-        
-        self.transformations = [CutPasteTransform(patch_size=cut_patch_size, use_flipping=True),
-                                CutPatchMeanTransform(patch_size=cut_patch_size), WhitenBlobTransform(width=(3, 8)),
-                                DarkenTransform(num_points=6, thickness=2), DarkenBlobTransform(width=(5, 18))]
-        
-        self.normalization = torchvision.transforms.Normalize(mean=[0.31945744]*3, std=[0.13307516]*3)
-        
-        self.train_accuracy = torchmetrics.classification.Accuracy(task="binary", num_classes=2)
-        self.train_precision = torchmetrics.classification.Precision(task="binary", num_classes=2)
-        self.train_recall = torchmetrics.classification.Recall(task="binary", num_classes=2)
-        
-        self.valid_accuracy = torchmetrics.classification.Accuracy(task="binary", num_classes=2)
-        self.valid_precision = torchmetrics.classification.Precision(task="binary", num_classes=2)
-        self.valid_recall = torchmetrics.classification.Recall(task="binary", num_classes=2)
-        
-        self.test_accuracy = torchmetrics.classification.Accuracy(task="binary", num_classes=2)
-        self.test_precision = torchmetrics.classification.Precision(task="binary", num_classes=2)
-        self.test_recall = torchmetrics.classification.Recall(task="binary", num_classes=2)
-            
-    def __applyCutAndPaste(self, batch: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
-        n, _, _, _ = batch.shape
-        labels = torch.zeros(n, device=self.device, dtype=torch.long)
-        
-        idxHalfWay = n // 2
-        randomChosenTransformationIdx = torch.randint(0, len(self.transformations), (1,))
-        batch[idxHalfWay:, ...] = self.transformations[randomChosenTransformationIdx](batch[idxHalfWay:, ...])
-        labels[idxHalfWay:, ...] = 1
-        
-        return torch.stack([batch[:, 0, ...]]*3, dim=1), torch.nn.functional.one_hot(labels, num_classes=2).to(torch.float)
-    
-    def forward(self, batch: torch.Tensor) -> torch.Tensor:
-        batch = self.normalization(batch)
-        x = self.transformer(batch)
-        return x
-  
-    def training_step(self, batch: torch.Tensor, batch_idx):
-        imgs, labels = self.__applyCutAndPaste(batch)
-        
-        predictions = self(imgs)
-        loss = torch.nn.functional.cross_entropy(predictions, labels)
-        
-        self.train_accuracy(predictions, labels)
-        self.train_precision(predictions, labels)
-        self.train_recall(predictions, labels)
-
-        return loss
-    
-    def on_train_epoch_end(self):
-        # log epoch metric
-        self.log('train_accuracy', self.train_accuracy)
-        self.log('train_precision', self.train_precision)
-        self.log('train_recall', self.train_recall)
-        
-    def validation_step(self, batch, batch_idx):
-        x, y = self.__applyCutAndPaste(batch)
-        y_hat = self(x)
-        val_loss = torch.nn.functional.cross_entropy(y_hat, y)
-        
-        self.valid_accuracy(y_hat, y)
-        self.valid_precision(y_hat, y)
-        self.valid_recall(y_hat, y)
-        
-        self.log('val_loss', val_loss, sync_dist=True)
-        self.log('val_accuracy', self.valid_accuracy, sync_dist=True)
-        self.log('val_precision', self.valid_precision, sync_dist=True)
-        self.log('val_recall', self.valid_recall, sync_dist=True)
-        return val_loss
-    
-    def test_step(self, batch, batch_idx):
-        x, y = self.__applyCutAndPaste(batch)
-        y_hat = self(x)
-        test_loss = torch.nn.functional.cross_entropy(y_hat, y)
-        self.test_accuracy(y_hat, y)
-        self.test_precision(y_hat, y)
-        self.test_recall(y_hat, y)
-        
-        self.log('test_loss', test_loss)
-        self.log('test_accuracy', self.test_accuracy)
-        self.log('test_precision', self.test_precision)
-        self.log('test_recall', self.test_recall)
-        return test_loss
-        
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=5e-4, weight_decay=1e-5)
-        return optimizer
- 
 
 class CutAndPaste(pl.LightningModule):
     def __init__(self, patch_size: int):
@@ -232,7 +139,7 @@ class CutAndPasteDataLoader(pl.LightningDataModule):
         self.num_workers = num_workers
         
     def setup(self, stage=None):
-        dataset = constructApollo17DatasetWOApollo(self.root, self.imgSize, self.stride)
+        dataset = constructApollo15DatasetWOApollo(self.root, self.imgSize, self.stride)
         print(tc.info + f"Got {len(dataset)} of images in total.")
         self.train, self.valid, self.test = random_split(dataset, (0.8, 0.1, 0.1))
 
@@ -257,7 +164,7 @@ if __name__ == "__main__":
     
     CutPasteModel = CutAndPaste(patch_size=cut_patch_size)
     
-    # logger = TensorBoardLogger("tb_logs", name="CutAndPaste", version="Apollo17")
+    # logger = TensorBoardLogger("tb_logs", name="CutAndPaste", version="Apollo15")
     # tr = pl.Trainer(logger=logger, strategy=DDPStrategy(), enable_checkpointing=True, default_root_dir="checkpoints", max_epochs=75)
     # tr.fit(model=CutPasteModel, datamodule=loader)
     # tr.test(model=CutPasteModel, datamodule=loader)
